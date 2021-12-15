67ba66aa8e5d449839533924d85efe0542c04513
Jose Pila <jose.pila@epn.edu.ec>
2021-12-14 20:45:45 -0500

    Update - Tarea 1

---
+++ Tareas/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+import kotlin.collections.ArrayList
+fun main(){
+}
+/*Variables a implementar
+* */
+abstract class Libros {
+    protected var idLibro: Int,
+    protected var Nombre: String,
+    protected var Autor: String,
+
+}
